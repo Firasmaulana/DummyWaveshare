4b88a558e7771d90f3f06387c4779c8a6e10e95f
Firasmaulana <147500489+Firasmaulana@users.noreply.github.com>
2026-04-28 15:53:18 +0700

    ganti ke kode example

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,84 @@
 #include <Arduino.h>
-#include <ModbusMaster.h>
 #include <lvgl.h>
 
-// --- LIBRARY DRIVER LAYAR WAVESHARE ---
+/*To use the built-in examples and demos of LVGL uncomment the includes below respectively.
+ *You also need to copy `lvgl/examples` to `lvgl/src/examples`. Similarly for the demos `lvgl/demos` to `lvgl/src/demos`.
+ Note that the `lv_examples` library is for LVGL v7 and you shouldn't install it for this version (since LVGL v8)
+ as the examples and demos are now part of the main LVGL library. */
+
+// #include <examples/lv_examples.h>
+// #include <demos/lv_demos.h>
+
+#define DIRECT_RENDER_MODE // Uncomment to enable full frame buffer
+
 #include <Arduino_GFX_Library.h>
-#include <Wire.h>
+
 #include "TCA9554.h"
+
+#include <Wire.h>
 #include "esp_lcd_touch_axs15231b.h"
 
-// --- DEFINISI PIN LCD & I2C ---
-#define GFX_BL        6
+#define GFX_BL 6  // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
+
 #define LCD_QSPI_CS   12
 #define LCD_QSPI_CLK  5
 #define LCD_QSPI_D0   1
 #define LCD_QSPI_D1   2
 #define LCD_QSPI_D2   3
 #define LCD_QSPI_D3   4
+
 #define I2C_SDA       8
 #define I2C_SCL       7
 
-// --- DEFINISI PIN MODBUS ---
-#define RS485_RX 43
-#define RS485_TX 44
-
-// --- INISIALISASI OBJEK HARDWARE ---
 TCA9554 TCA(0x20);
+
 Arduino_DataBus *bus = new Arduino_ESP32QSPI(LCD_QSPI_CS, LCD_QSPI_CLK, LCD_QSPI_D0, LCD_QSPI_D1, LCD_QSPI_D2, LCD_QSPI_D3);
+
 Arduino_GFX *gfx = new Arduino_AXS15231B(bus, -1 /* RST */, 0 /* rotation */, false, 320, 480);
 
-HardwareSerial RS485Serial(1); 
-ModbusMaster node;
 
-// --- VARIABEL LVGL V8 & MODBUS ---
 uint32_t screenWidth;
 uint32_t screenHeight;
-lv_disp_draw_buf_t draw_buf; // V8 menggunakan ini
+uint32_t bufSize;
+lv_disp_draw_buf_t draw_buf;
 lv_color_t *disp_draw_buf1;
 lv_color_t *disp_draw_buf2;
+lv_disp_drv_t disp_drv;
 
-unsigned long lastModbusPoll = 0;
-const unsigned long POLLING_INTERVAL = 2000;
+#if LV_USE_LOG != 0
+/* Serial debugging */
+void my_print(const char *buf) {
+  Serial.printf(buf);
+  Serial.flush();
+}
+#endif
 
-// Objek UI
-lv_obj_t * ui_LabelGas;
-lv_obj_t * ui_LabelSuhu;
-lv_obj_t * ui_LabelKelembaban;
-lv_obj_t * ui_LabelStatus;
-
-// --- CALLBACK LVGL V8 ---
-// Flush callback (Render layar)
+/* Display flushing */
 void my_disp_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p) {
   uint32_t w = (area->x2 - area->x1 + 1);
   uint32_t h = (area->y2 - area->y1 + 1);
+
+#if (LV_COLOR_16_SWAP != 0)
+  gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#else
   gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+#endif
+
   lv_disp_flush_ready(disp_drv);
 }
 
-// Read callback (Touchscreen)
-void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
+/*Read the touchpad*/
+void my_touchpad_read(lv_indev_drv_t *indev_drv, lv_indev_data_t *data) {
+
   touch_data_t touch_data;
   bsp_touch_read();
+
+  // int16_t x[1], y[1];
+  // uint8_t touched = touch.getPoint(x, y, 1);
+
   if (bsp_touch_get_coordinates(&touch_data)) {
     data->state = LV_INDEV_STATE_PR;
+    /*Set the coordinates*/
     data->point.x = touch_data.coords[0].x;
     data->point.y = touch_data.coords[0].y;
   } else {
@@ -69,130 +86,130 @@ void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
   }
 }
 
-// --- FUNGSI PEMBUAT UI ---
-void build_ui() {
-  lv_obj_t * scr = lv_scr_act();
-  lv_obj_set_style_bg_color(scr, lv_color_hex(0x1A1A1A), LV_PART_MAIN);
-
-  lv_obj_t * panel = lv_obj_create(scr);
-  lv_obj_set_size(panel, 280, 300);
-  lv_obj_align(panel, LV_ALIGN_CENTER, 0, 0);
-
-  lv_obj_t * title = lv_label_create(panel);
-  lv_label_set_text(title, "AQMS MONITORING");
-  lv_obj_align(title, LV_ALIGN_TOP_MID, 0, 10);
-  lv_obj_set_style_text_color(title, lv_color_hex(0x00A8FF), LV_PART_MAIN);
-
-  ui_LabelGas = lv_label_create(panel);
-  lv_label_set_text(ui_LabelGas, "Gas : Menunggu...");
-  lv_obj_align(ui_LabelGas, LV_ALIGN_TOP_LEFT, 20, 60);
-
-  ui_LabelSuhu = lv_label_create(panel);
-  lv_label_set_text(ui_LabelSuhu, "Suhu: Menunggu...");
-  lv_obj_align(ui_LabelSuhu, LV_ALIGN_TOP_LEFT, 20, 100);
-
-  ui_LabelKelembaban = lv_label_create(panel);
-  lv_label_set_text(ui_LabelKelembaban, "RH  : Menunggu...");
-  lv_obj_align(ui_LabelKelembaban, LV_ALIGN_TOP_LEFT, 20, 140);
-
-  ui_LabelStatus = lv_label_create(panel);
-  lv_label_set_text(ui_LabelStatus, "Stat: Menghubungkan...");
-  lv_obj_align(ui_LabelStatus, LV_ALIGN_TOP_LEFT, 20, 180);
-}
-
 void setup() {
-  Serial.begin(115200);
+#ifdef DEV_DEVICE_INIT
+  DEV_DEVICE_INIT();
+#endif
 
   Wire.begin(I2C_SDA, I2C_SCL);
   TCA.begin();
   TCA.pinMode1(1, OUTPUT);
-  TCA.write1(1, 1); delay(10);
-  TCA.write1(1, 0); delay(10);
-  TCA.write1(1, 1); delay(200);
+  TCA.write1(1, 1);
+  delay(10);
+  TCA.write1(1, 0);
+  delay(10);
+  TCA.write1(1, 1);
+  delay(200);
 
-  bsp_touch_init(&Wire, -1, 0, 320, 480);
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+  // while(!Serial);
+  Serial.println("Arduino_GFX LVGL_Arduino_v8 example ");
+  String LVGL_Arduino = String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
+  Serial.println(LVGL_Arduino);
 
+  // Init Display
   if (!gfx->begin()) {
     Serial.println("gfx->begin() failed!");
   }
   gfx->fillScreen(RGB565_BLACK);
-  
+
+#ifdef GFX_BL
   pinMode(GFX_BL, OUTPUT);
   digitalWrite(GFX_BL, HIGH);
+#endif
 
-  // --- INISIALISASI LVGL V8 ---
+  // Init touch device
+
+  
+
+  // if (!touch.begin(Wire, FT6X36_SLAVE_ADDRESS)) {
+  //   Serial.println("Failed to find FT6X36 - check your wiring!");
+  //   while (1) {
+  //     delay(1000);
+  //   }
+  // }
+
   lv_init();
+
+#if LV_USE_LOG != 0
+  lv_log_register_print_cb(my_print); /* register print function for debugging */
+#endif
 
   screenWidth = gfx->width();
   screenHeight = gfx->height();
-  
-  // Alokasi Buffer untuk V8 (Lebih hemat memori)
-  uint32_t bufSize = screenWidth * 40; // Render 40 baris per frame
-  disp_draw_buf1 = (lv_color_t *)heap_caps_malloc(bufSize * sizeof(lv_color_t), MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-  
-  if (!disp_draw_buf1) {
-    Serial.println("Alokasi memori LVGL gagal!");
-  } else {
-    // 1. Inisialisasi Draw Buffer
-    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf1, NULL, bufSize);
 
-    // 2. Inisialisasi Display Driver V8
-    static lv_disp_drv_t disp_drv;
+  bsp_touch_init(&Wire, -1, 0, screenWidth, screenHeight);
+
+#ifdef DIRECT_RENDER_MODE
+  bufSize = screenWidth * screenHeight;
+#else
+  bufSize = screenWidth * 80;
+#endif
+
+  disp_draw_buf1 = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_8BIT);
+  disp_draw_buf2 = (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_8BIT);
+
+  if (!disp_draw_buf1 || !disp_draw_buf2) {
+    Serial.println("LVGL disp_draw_buf allocate failed!");
+  } else {
+    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf1, disp_draw_buf2, bufSize);
+    /* Initialize the display */
     lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
     disp_drv.hor_res = screenWidth;
     disp_drv.ver_res = screenHeight;
     disp_drv.flush_cb = my_disp_flush;
     disp_drv.draw_buf = &draw_buf;
+#ifdef DIRECT_RENDER_MODE
+    disp_drv.full_refresh = true;
+#endif
     lv_disp_drv_register(&disp_drv);
 
-    // 3. Inisialisasi Input Device V8
+    /* Initialize the (dummy) input device driver */
     static lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb = my_touchpad_read;
     lv_indev_drv_register(&indev_drv);
+
+    /* Option 1: Create simple label */
+    lv_obj_t *label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "Hello Arduino! (V" GFX_STR(LVGL_VERSION_MAJOR) "." GFX_STR(LVGL_VERSION_MINOR) "." GFX_STR(LVGL_VERSION_PATCH) ")");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+
+    lv_obj_t *sw = lv_switch_create(lv_scr_act());
+    lv_obj_align(sw, LV_ALIGN_TOP_MID, 0, 50);
+
+    sw = lv_switch_create(lv_scr_act());
+    lv_obj_align(sw, LV_ALIGN_BOTTOM_MID, 0, -50);
+
+    /* Option 2: Try an example. See all the examples
+     * online: https://docs.lvgl.io/master/examples.html
+     * source codes: https://github.com/lvgl/lvgl/tree/e7f88efa5853128bf871dde335c0ca8da9eb7731/examples */
+    // lv_example_btn_1();
+
+    /* Option 3: Or try out a demo. Don't forget to enable the demos in lv_conf.h. E.g. LV_USE_DEMOS_WIDGETS*/
+    // lv_demo_widgets();
+    // lv_demo_benchmark();
+    // lv_demo_keypad_encoder();
+    // lv_demo_music();
+    // lv_demo_stress();
   }
 
-  build_ui();
-
-  RS485Serial.begin(9600, SERIAL_8N1, RS485_RX, RS485_TX);
-  node.begin(1, RS485Serial); 
+  Serial.println("Setup done");
 }
 
 void loop() {
-  lv_timer_handler(); // V8 menggunakan lv_timer_handler, BUKAN lv_task_handler
-  delay(5); 
-  
-  unsigned long currentMillis = millis();
-  if (currentMillis - lastModbusPoll >= POLLING_INTERVAL) {
-    lastModbusPoll = currentMillis; 
-    
-    uint8_t result = node.readHoldingRegisters(0x0000, 4);
+  lv_timer_handler(); /* let the GUI do its work */
 
-    if (result == node.ku8MBSuccess) {
-      float gasConcentration = node.getResponseBuffer(0) / 10.0;
-      float temperature      = node.getResponseBuffer(1) / 10.0;
-      float humidity         = node.getResponseBuffer(2) / 10.0;
-      uint16_t rawStatus     = node.getResponseBuffer(3);
+// #ifdef DIRECT_RENDER_MODE
+// #if (LV_COLOR_16_SWAP != 0)
+//   gfx->draw16bitBeRGBBitmap(0, 0, (uint16_t *)disp_draw_buf, screenWidth, screenHeight);
+// #else
+//   gfx->draw16bitRGBBitmap(0, 0, (uint16_t *)disp_draw_buf, screenWidth, screenHeight);
+// #endif
+// #endif  // !DIRECT_RENDER_MODE
 
-      if (ui_LabelGas != NULL) {
-        lv_label_set_text_fmt(ui_LabelGas, "Gas : %.1f ppm", gasConcentration);
-        lv_label_set_text_fmt(ui_LabelSuhu, "Suhu: %.1f °C", temperature);
-        lv_label_set_text_fmt(ui_LabelKelembaban, "RH  : %.1f %%", humidity);
-        
-        if (rawStatus == 0) {
-            lv_label_set_text(ui_LabelStatus, "Stat: OK (Online)");
-            lv_obj_set_style_text_color(ui_LabelStatus, lv_color_hex(0x00FF00), LV_PART_MAIN);
-        } else {
-            lv_label_set_text(ui_LabelStatus, "Stat: WARNING");
-            lv_obj_set_style_text_color(ui_LabelStatus, lv_color_hex(0xFFA500), LV_PART_MAIN);
-        }
-      }
-    } else {
-      if (ui_LabelStatus != NULL) {
-         lv_label_set_text(ui_LabelStatus, "Stat: DISCONNECTED");
-         lv_obj_set_style_text_color(ui_LabelStatus, lv_color_hex(0xFF0000), LV_PART_MAIN);
-      }
-    }
-  }
+  delay(5);
 }
